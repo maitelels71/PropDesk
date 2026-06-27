f883f196f7bfcd8872ad9c30ba766eb9dfd0f111
maitelels71 <maitelels@gmail.com>
2026-06-26 21:36:46 -0400

    feat(ui): add EquityChartView code-behind

---
+++ src/PropDesk.UI/Views/EquityChartView.xaml.cs
@@ -0,0 +1,11 @@
+using System.Windows.Controls;
+
+namespace PropDesk.UI.Views;
+
+public partial class EquityChartView : UserControl
+{
+    public EquityChartView()
+    {
+        InitializeComponent();
+    }
+}
